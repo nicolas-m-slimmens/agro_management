09e338ecf3fbf2b73923f5b56dac080a34940f6c
Prelang Builder <builder@prelang.com>
2016-08-01 01:35:45 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'SoilSurvey' to allow for create, update

--- app/admin/soil_survey.rb
+++ app/admin/soil_survey.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register SoilSurvey do
+  permit_params :organic_carbon, :organic_material, :total_nitrogen, :nitrate, :nitrate_nitrogens, :comparable_fosforo, :calcium, :magnesium, :ph, :carbon_nitrogen, :calcium_magnesium, :conductivity, :sulfur, :hardness_of_water, :water_ph
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
