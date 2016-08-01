892763de78ef5da61070c786f6892544a5613a94
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:25 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Field' to allow for create, update

--- app/admin/field.rb
+++ app/admin/field.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Field do
+  permit_params :name, :address, :locality, :province, :country, :area, :own, :observations, :owner_contact, :manager_contact
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
