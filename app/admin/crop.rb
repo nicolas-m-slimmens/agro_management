8bd25875675ff28f67b149672a237e5d8abcb63d
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:04 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Crop' to allow for create, update

--- app/admin/crop.rb
+++ app/admin/crop.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Crop do
+  permit_params :name, :unit
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
