5ede5f31ecefe13c60c68dc62086cd2e889e4e59
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Provider' to allow for create, update

--- app/admin/provider.rb
+++ app/admin/provider.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Provider do
+  permit_params :name, :tax_identifier, :observations
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
