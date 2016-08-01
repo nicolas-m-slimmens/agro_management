83f618073310c0626176ca8eaf5ed3f64769dd50
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:35 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Contact' to allow for create, update

--- app/admin/contact.rb
+++ app/admin/contact.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Contact do
+  permit_params :name, :last_name, :telephone_number, :address, :locality, :province, :email_address, :observations
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
