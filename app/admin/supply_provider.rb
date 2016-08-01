16adccb8484c7af3b9071c57185f077f0bf16e94
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:18 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'SupplyProvider' to allow for create, update

--- app/admin/supply_provider.rb
+++ app/admin/supply_provider.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register SupplyProvider do
+  permit_params :price
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
