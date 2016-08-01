7f8e025abc5d2cc7fdf38ca8fdc5164023972b89
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:07 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Fertilization' to allow for create, update

--- app/admin/fertilization.rb
+++ app/admin/fertilization.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Fertilization do
+  permit_params :campaing, :type, :applied_at, :affected_area, :units_per_hectare, :price_per_unit, :observations
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
