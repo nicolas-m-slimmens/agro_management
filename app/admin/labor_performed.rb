32f2d2b1169f4d4192935440ee020e1e30c5a0a3
Prelang Builder <builder@prelang.com>
2016-08-01 01:35:54 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'LaborPerformed' to allow for create, update

--- app/admin/labor_performed.rb
+++ app/admin/labor_performed.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register LaborPerformed do
+  permit_params :campaign, :price, :done_at, :observations
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
