8241420c5f818b1347eb3563cd24b9b951d0ef62
Prelang Builder <builder@prelang.com>
2016-08-01 01:36:31 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Production' to allow for create, update

--- app/admin/production.rb
+++ app/admin/production.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Production do
+  permit_params :variety, :quality, :quantity, :area, :own_storage, :campaign
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
